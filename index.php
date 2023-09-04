cc7f098a6d14208362953bc92409d4fc7f52fb68
caio <caio.buono8@gmail.com>
2023-09-03 21:21:13 -0300

    primeira versão

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caixa Eletrônico</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Caixa Eletrônico</h1>
+        <form action="" method="get">
+            <label for="dinheiro">Qual valor você deseja sacar? (R$)*</label>
+            <input type="number" name="dinheiro" id="dinheiro">
+            <p>* Notas disponíveis: R$100, R$50, R$10 e R$5</p>
+            <input type="submit" value="Sacar">
+        </form>
+    </main>
+</body>
+</html>
